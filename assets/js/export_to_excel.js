5f6749b4b57bdf43b10f5bd3228d02787b3d47cd
danielpilla <daniel.b.pilla@gmail.com>
2020-01-28 21:30:20 -0500

    Update export_to_excel.js

--- assets/js/export_to_excel.js
+++ assets/js/export_to_excel.js
@@ -1,13 +1,11 @@
-function startExport(id) {
-        var wb = XLSX.utils.table_to_book(document.getElementById(id), {sheet:"Sheet JS"});
-        var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
-        function s2ab(s) {
-                var buf = new ArrayBuffer(s.length);
-                var view = new Uint8Array(buf);
-                for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-                return buf;
-        }
-        function exportToExcel(tableID) {
+var wb = XLSX.utils.table_to_book(document.getElementById('playbook'), {sheet:"Sheet JS"});
+var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+}
+function exportToExcel(tableID) {
         saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'qs-admin-playbook.xlsx');
-        }
 }
