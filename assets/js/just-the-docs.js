e25e9b95970c87a4388a2f82c1e5b2ac4bebc207
Jesse Paris <jparis@gmail.com>
2020-03-27 14:47:55 -0700

    Update just-the-docs.js

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -67,6 +67,7 @@ function initSearch() {
         this.metadataWhitelist = ['position']
 
         for (var i in data) {
+          data[i].content = data[i].content.replace(/#monthly/g, "monthlySearchTerm");
           this.add({
             id: i,
             title: data[i].title,
@@ -94,6 +95,7 @@ function initSearch() {
     var index = index;
     var docs = data;
     var searchInput = document.querySelector('.js-search-input');
+    searchInput = searchInput.replace(/#monthly/g, "monthlySearchTerm");
     var searchResults = document.querySelector('.js-search-results');
 
     function hideResults() {
